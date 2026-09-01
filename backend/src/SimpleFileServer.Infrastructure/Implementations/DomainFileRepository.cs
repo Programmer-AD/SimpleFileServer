68b57cb2152cc1915ec0281ba5f1f5ba9f502dd1
Andrey <andrey08112003@gmail.com>
2026-09-02 00:24:28 +0300

    Finalize backend

--- backend/src/SimpleFileServer.Infrastructure/Implementations/DomainFileRepository.cs
+++ backend/src/SimpleFileServer.Infrastructure/Implementations/DomainFileRepository.cs
@@ -1,14 +1,53 @@
+using Microsoft.EntityFrameworkCore;
 using SimpleFileServer.Application.Abstractions.Infrastructure;
 using SimpleFileServer.Domain.Entities;
+using SimpleFileServer.Infrastructure.EF;
 
 namespace SimpleFileServer.Infrastructure.Implementations;
 
-#warning "Not implemened"
-internal class DomainFileRepository : IDomainFileRepository
+internal class DomainFileRepository(
+    AppDbContext appDbContext
+) : IDomainFileRepository
 {
-    public Task<Guid> CreateAsync(DomainFile domainFile) => throw new NotImplementedException();
-    public Task<DomainFile?> GetAsync(Guid id) => Task.FromResult((DomainFile?)null);
-    public Task<List<DomainFile>> GetAllAsync() => throw new NotImplementedException();
-    public Task UpdateAsync(DomainFile domainFile) => throw new NotImplementedException();
-    public Task DeleteAsync(Guid id) => throw new NotImplementedException();
+    private DbSet<DomainFile> DomainFileSet => appDbContext.DomainFiles;
+
+    public async Task<Guid> CreateAsync(DomainFile domainFile)
+    {
+        domainFile.Id = Guid.CreateVersion7();
+
+        DomainFileSet.Add(domainFile);
+        await appDbContext.SaveChangesAsync();
+
+        return domainFile.Id;
+    }
+
+    public async Task<DomainFile?> GetAsync(Guid id)
+    {
+        DomainFile? result = await DomainFileSet.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        return result;
+    }
+
+    public async Task<List<DomainFile>> GetAllAsync()
+    {
+        List<DomainFile> result = await DomainFileSet.AsNoTracking().ToListAsync();
+        return result;
+    }
+
+    public async Task UpdateAsync(DomainFile domainFile)
+    {
+        DomainFileSet.Update(domainFile);
+        await appDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        DomainFile? domainFile = await DomainFileSet.FindAsync(id);
+        if (domainFile == null)
+        {
+            return;
+        }
+
+        DomainFileSet.Remove(domainFile);
+        await appDbContext.SaveChangesAsync();
+    }
 }
